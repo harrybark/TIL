8ac97b36d218f6ef520b757e394884a56aab830a
harrybark <harrypmw@gmail.com>
2023-05-24 23:03:23 +0900

    feat.GoF Design Pattern - Facade Pattern

---
+++ design-patterns/src/main/java/me/harry/designpatterns/designpatterns/structural_patterns/facade/after/Client.java
@@ -0,0 +1,19 @@
+package me.harry.designpatterns.designpatterns.structural_patterns.facade.after;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        EmailSettings emailSettings = new EmailSettings();
+        emailSettings.setHost("127.0.0.1");
+
+        EmailSender emailSender = new EmailSender(emailSettings);
+        EmailMessage emailMessage = new EmailMessage();
+        emailMessage.setFrom("keesun");
+        emailMessage.setTo("whiteship");
+        emailMessage.setSubject("오징어게임");
+        emailMessage.setText("밖은 더 지옥어디라고..");
+
+        emailSender.sendEmail(emailMessage);
+    }
+}
---
+++ design-patterns/src/main/java/me/harry/designpatterns/designpatterns/structural_patterns/facade/after/EmailSettings.java
@@ -0,0 +1,14 @@
+package me.harry.designpatterns.designpatterns.structural_patterns.facade.after;
+
+public class EmailSettings {
+
+    private String host;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
