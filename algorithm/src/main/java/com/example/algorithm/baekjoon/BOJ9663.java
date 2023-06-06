9d433b3f716c58f7bf448d30a8a3329f237be960
harrybark <harrypmw@gmail.com>
2023-06-06 23:01:28 +0900

    feat.BOJ 9663

---
+++ algorithm/src/main/java/com/example/algorithm/baekjoon/BOJ9663.java
@@ -0,0 +1,57 @@
+package com.example.algorithm.baekjoon;
+
+/***
+ * @Author : 갈색토마토
+ * @Date   : 2023.06.06 (Tue)
+ * @Description : BAEKJOON No.9663
+ * @URL         : https://www.acmicpc.net/problem/9663
+ */
+public class BOJ9663 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static void input() {
+        N = scan.nextInt();
+        col = new int[N + 1];
+    }
+
+    static int N, ans;
+    static int[] col;
+
+    static boolean attackable(int r1, int c1, int r2, int c2) {
+        if (c1 == c2) return true;
+        if (r1 - c1 == r2 - c2) return true;
+        if (r1 + c1 == r2 + c2) return true;
+        return false;
+    }
+
+    static void rec_func(int row) {
+        if (row == N + 1) {
+            ans++;
+        } else {
+            for (int c = 1; c <= N; c++) {
+                boolean possible = true;
+                // valid check (row, c)
+                for (int i=1;i<=row-1;i++){
+                    // (i, col[i])
+                    if (attackable(row, c, i, col[i])){
+                        possible = false;
+                        break;
+                    }
+                }
+                if (possible) {
+                    col[row] = c;
+                    rec_func(row + 1);
+                    col[row] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        // 1 번째 원소부터 M 번째 원소를 조건에 맞게 고르는 모든 방법을 탐색해줘
+        rec_func(1);
+        System.out.println(ans);
+    }
+}
