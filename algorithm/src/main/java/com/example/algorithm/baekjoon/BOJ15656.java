f27f84053c01082148eaf974889087aa0fa12842
harrybark <harrypmw@gmail.com>
2023-06-09 00:07:11 +0900

    feat.BOJ 15656

--- algorithm/src/main/java/com/example/algorithm/baekjoon/BOJ15656.java
+++ algorithm/src/main/java/com/example/algorithm/baekjoon/BOJ15656.java
@@ -1,22 +1,47 @@
 package com.example.algorithm.baekjoon;
 
+import java.util.Arrays;
+
 public class BOJ15656 {
 
     static FastReader scan = new FastReader();
     static StringBuffer sb = new StringBuffer();
 
     static int N, M;
-    static int[] selected;
+    static int[] selected, basket;
 
     static void init() {
+        N = scan.nextInt();
+        M = scan.nextInt();
 
+        basket = new int[N + 1];
+        selected = new int[M + 1];
+
+        for (int i = 1 ; i <= N ; i += 1) {
+            basket[i] = scan.nextInt();
+        }
+        Arrays.sort(basket);
     }
 
     static void rec_func(int row) {
 
+        if ( row == M + 1 ) {
+            for ( int i = 1 ; i <= M ; i += 1 ) {
+                sb.append(selected[i]).append(" ");
+            }
+            sb.append("\n");
+        } else {
+            for ( int cand = 1; cand <= N ; cand += 1 ) {
+                selected[row] = basket[cand];
+                rec_func(row + 1);
+                selected[row] = 0;
+            }
+        }
     }
 
     public static void main(String[] args) {
-
+        init();
+        rec_func(1);
+        System.out.println(sb.toString());
     }
 }
