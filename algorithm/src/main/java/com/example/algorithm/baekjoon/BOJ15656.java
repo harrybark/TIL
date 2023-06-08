607681baf66cf6329ce3cc715c0d1e23f3de5a1e
harrybark <harrypmw@gmail.com>
2023-06-08 23:41:51 +0900

    feat.BOJ 15656

---
+++ algorithm/src/main/java/com/example/algorithm/baekjoon/BOJ15656.java
@@ -0,0 +1,22 @@
+package com.example.algorithm.baekjoon;
+
+public class BOJ15656 {
+
+    static FastReader scan = new FastReader();
+    static StringBuffer sb = new StringBuffer();
+
+    static int N, M;
+    static int[] selected;
+
+    static void init() {
+
+    }
+
+    static void rec_func(int row) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
