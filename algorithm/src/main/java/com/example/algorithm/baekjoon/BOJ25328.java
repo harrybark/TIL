14457e3beee9c8ea3036f6b61679f2e3a292d8b1
harrybark <harrypmw@gmail.com>
2023-06-20 23:53:07 +0900

    feat.BOJ25328

---
+++ algorithm/src/main/java/com/example/algorithm/baekjoon/BOJ25328.java
@@ -0,0 +1,24 @@
+package com.example.algorithm.baekjoon;
+
+public class BOJ25328 {
+
+    static FastReader scan = new FastReader();
+    static String X,Y,Z;
+    static int k;
+
+    static void input() {
+        X = scan.next();
+        Y = scan.next();
+        Z = scan.next();
+        k = scan.nextInt();
+    }
+
+    static void dfs(int idx) {
+
+    }
+
+    public static void main(String[] args) {
+        input();
+        
+    }
+}
