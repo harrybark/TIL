4f3918d12231ab8ca89264d3753019a8f8f969c0
harrybark <harrypmw@gmail.com>
2023-08-15 23:21:00 +0900

    feat.add BOJ18429

--- algorithm/src/main/java/com/example/algorithm/baekjoon/BOJ18429.java
+++ algorithm/src/main/java/com/example/algorithm/baekjoon/BOJ18429.java
@@ -1,4 +1,11 @@
 package com.example.algorithm.baekjoon;
 
+/***
+ * @Author : 갈색토마토
+ * @Date   : 2023.07.01 (Sat)
+ * @Description : BAEKJOON No.18429
+ * @URL         : https://www.acmicpc.net/problem/18429
+ */
 public class BOJ18429 {
+
 }
