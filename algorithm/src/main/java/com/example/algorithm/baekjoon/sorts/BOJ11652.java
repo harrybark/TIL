8d9f487f0c5468dac2ffdfbe3c6d83cd931cf4fc
harrybark <harrypmw@gmail.com>
2023-08-19 23:35:16 +0900

    feat.Add BOJ11652

---
+++ algorithm/src/main/java/com/example/algorithm/baekjoon/sorts/BOJ11652.java
@@ -0,0 +1,10 @@
+package com.example.algorithm.baekjoon.sorts;
+
+/***
+ * @Author : 갈색토마토
+ * @Date   : 2023.08.19 (Sat)
+ * @Description : BAEKJOON No.11652
+ * @URL         : https://www.acmicpc.net/problem/11652
+ */
+public class BOJ11652 {
+}
