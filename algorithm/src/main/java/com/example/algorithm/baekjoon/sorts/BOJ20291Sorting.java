287bbb5b7a87d57fc92579542056975676d9e9df
harrybark <harrypmw@gmail.com>
2023-10-05 22:13:44 +0900

    feat.BOJ20291

--- algorithm/src/main/java/com/example/algorithm/baekjoon/sorts/BOJ20291Sorting.java
+++ algorithm/src/main/java/com/example/algorithm/baekjoon/sorts/BOJ20291Sorting.java
@@ -1,4 +1,60 @@
 package com.example.algorithm.baekjoon.sorts;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class BOJ20291Sorting {
+
+    public static void main(String[] args) {
+        System.out.println("answer : " + new BOJ20291Sorting().solution(new int[]{0,1,0,0}, 1));
+    }
+
+
+
+    public int solution(int[] student, int k) {
+        int answer = -1;
+        int idx = student.length - 1;
+        int[] repStr = null;
+
+        Set<String> set = new HashSet<>();
+        StringBuffer sb = new StringBuffer();
+
+        for ( int i = idx ; i >= k ; i -= 1 ) {
+            sb.delete(0, sb.length());
+            int cur = idx - i + 1;
+            System.out.println(idx + " / " + i + " / " + cur);
+
+            for ( int start = cur ; start > 0 ; start -= 1 ) {
+
+                repStr = Arrays.copyOfRange(student, 0, student.length - start);
+                for ( int c = (repStr.length-1) - i ; c < repStr.length ; c += 1 ) {
+                    sb.append(repStr[c]);
+                }
+                System.out.println(start + " copy : " + Arrays.toString(repStr));
+
+                set.add(sb.toString());
+            }
+        }
+/*
+        String[] strArr = set.toArray(new String[]{});
+
+        for (int i = 0 ; i < strArr.length ; i += 1 ) {
+            char[] chArr = strArr[i].toCharArray();
+            int kCnt = 0;
+            for ( int j = 0 ; j < chArr.length; j += 1 ) {
+                if ( chArr[j] == '1' ) {
+                    kCnt += 1;
+                }
+            }
+
+            if ( kCnt == k ) {
+                answer += 1;
+            }
+        }
+*/
+        return answer;
+    }
+
 }
